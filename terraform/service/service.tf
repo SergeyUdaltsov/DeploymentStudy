18f265713bda808250c5c9d570ad2c718a47606e
SergeyUdaltsov <sergii.udaltsov@gmail.com>
2025-06-13 13:28:17 +0300

    destroy fixed

--- terraform/service/service.tf
+++ terraform/service/service.tf
@@ -37,7 +37,7 @@ resource "aws_ecs_task_definition" "task_definition" {
   container_definitions = jsonencode([
     {
       name      = local.container_name
-      image     = "143936507261.dkr.ecr.eu-central-1.amazonaws.com/j3-repository-${var.region}-${var.env}:${var.image_tag}"
+      image     = "143936507261.dkr.ecr.${var.region}.amazonaws.com/j3-repository-${var.region}-${var.env}:${var.image_tag}"
       essential = true
       portMappings = [
         {
@@ -50,7 +50,7 @@ resource "aws_ecs_task_definition" "task_definition" {
         logDriver = "awslogs"
         options = {
           "awslogs-group"         = data.terraform_remote_state.infra-data.outputs.log_group_name
-          "awslogs-region"        = "eu-central-1"
+          "awslogs-region"        = var.region
           "awslogs-stream-prefix" = "j3-study"
         }
       }
